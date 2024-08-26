882517af855acc870ea860d9a6329743555b5033
zzq89757 <1604653835@qq.com>
2024-08-26 16:26:21 +0800

    add agct peak

--- bio_test.py
+++ bio_test.py
@@ -1,11 +1,15 @@
 from Bio import SeqIO
 
-seq = SeqIO.parse("/home/wayne/Project/SC/Sanger/B103-(T3389)pUp-pDown-flank-R.ab1","abi")
+# seq = SeqIO.parse("/home/wayne/Project/SC/Sanger/B103-(T3389)pUp-pDown-flank-R.ab1","abi")
 
 # print(dir(seq))
 
-for s in seq:
-  print(s.name)
-  print(s.seq)
-  # phred_quality
-  print(s.letter_annotations['phred_quality'])
+for seq in SeqIO.parse("/home/wayne/Project/SC/Sanger/B103-(T3389)pUp-pDown-flank-R.ab1","abi"):
+        seq_id, sequence, qual_array = seq.name, seq.seq, seq.letter_annotations['phred_quality']
+        abif_raw = seq.annotations["abif_raw"]
+        data_g = list(abif_raw["DATA9"])
+        data_a = list(abif_raw["DATA10"])
+        data_t = list(abif_raw["DATA11"])
+        data_c = list(abif_raw["DATA12"])
+
+        print(data_g)
--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,22 @@
 from Bio import SeqIO
 
+def trim_seq(seq:str):
+    ...
 
-def parse_abi_file(file_path):
+
+def parse_abi_file(file_path:str) -> None:
     for seq in SeqIO.parse(file_path,"abi"):
-        print(seq.name)
-        print(seq.seq)
+        seq_id, sequence, qual_array = seq.name, seq.seq, seq.letter_annotations['phred_quality']
+        abif_raw = seq.annotations["abif_raw"]
+        data_g = list(abif_raw["DATA9"])
+        data_a = list(abif_raw["DATA10"])
+        data_t = list(abif_raw["DATA11"])
+        data_c = list(abif_raw["DATA12"])
+
+        print(data_g)
 
 
-def main():
+def main() -> None:
     input_file = "/home/wayne/Project/SC/Sanger/B103-(T3389)pUp-pDown-flank-R.ab1"
     parse_abi_file(input_file)
     
