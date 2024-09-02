e0a6bf52af6c9dfc36e33e38d154c6666ce099f2
zzq89757 <1604653835@qq.com>
2024-09-02 11:20:48 +0800

    add comment

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,20 @@
 from collections import defaultdict
 from Bio import SeqIO,Align
 
+
+def parse_fq_ref_map_table() -> defaultdict:
+    '''将下机数据与其对应参考的映射关系存入字典'''
+
+def generate_ref(vector_seq:str, sg_table:str):
+    '''根据原始载体和sgRNA序列生成不同的参考文件'''
+    ...
+
 def peak_evaluation():
-    '''检测重叠峰'''
+    '''检测下机数据重叠峰'''
     ...
 
 def trim_static(seq_record, start:int=50, end:int=800):
-    '''固定保留50-800部分'''
+    '''裁剪下机数据，固定保留50-800部分'''
     return seq_record.seq[50:800],seq_record.letter_annotations["phred_quality"][50:800]
     
 
@@ -30,6 +38,7 @@ def trim_seq_by_qual(seq_record, threshold=20):
 
 
 def data_from_abi(file_path:str) -> defaultdict:
+    '''读取ab1文件并将序列信息存入字典'''
     data_dict = defaultdict(lambda:defaultdict())
     for seq in SeqIO.parse(file_path,"abi"):
         seq_id= seq.name
@@ -46,7 +55,10 @@ def data_from_abi(file_path:str) -> defaultdict:
         data_dict[seq_id]['qual'] = trimmed_qual
     return data_dict
 
-def align2ref():
+def align2ref(ref_file:str, data_dict:defaultdict) -> None:
+    '''构建参考序列索引并将下机数据比对到参考'''
+    from os import system
+    system(f"bwa index {ref_file}")
     ...
 
 
