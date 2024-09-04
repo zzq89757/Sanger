5ef191632715fc254060d63ae4e411e433b7491c
zzq89757 <1604653835@qq.com>
2024-09-04 15:07:16 +0800

    multiple process start

---
+++ multiple_process.py
@@ -0,0 +1,17 @@
+# 提取同一个well中的多个ab1文件中的序列信息 保存为fq后 bwa比对 处理bam文件
+
+def extract_data():
+    ...
+
+
+def construct_fq():
+    ...
+    
+    
+def detective_alignment_result():
+    ...
+    
+    
+def process_alignment():
+    ...
+    
